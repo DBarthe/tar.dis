4f3b2a7faa98edf3303f1d431d9ec0677ee7e86a
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2014-11-23 21:01:24 +0100

    Module PrefixCode

---
+++ src/prefixCode.ml
@@ -0,0 +1,42 @@
+(**
+  Module used to represent and use prefix codes for 8-bits words.
+
+  @author Barthelemy Delemotte
+*)
+
+type word = bool list
+
+type t = word array
+
+exception Coding_undef
+
+let empty () = Array.make 256 []
+
+let check_sym sym = 
+  if sym < 0 || sym > 255 then
+    raise (Invalid_argument "bad symbol")
+  else ()
+
+let set code sym coding =
+  check_sym sym ;
+  code.(sym) <- coding
+
+let unset code sym =
+  set code sym []
+
+let encode_sym code sym =
+  check_sym sym ;
+  match code.(sym) with
+  | [] -> raise Coding_undef
+  | c -> c
+
+(* tail-recursive version of flatten *)
+let flatten_opti l =
+  let rec aux accu = function
+  | [] -> List.rev accu
+  | xs::xss -> aux (List.rev_append xs accu) xss
+  in aux [] l 
+
+let encode_source code src =
+  let words = List.map (encode_sym code) src in
+  flatten_opti words
---
+++ src/prefixCode.mli
@@ -0,0 +1,29 @@
+(**
+  Module used to represent and use prefix codes for 8-bits words.
+
+  @author Barthelemy Delemotte
+*)
+
+(** The type [t] represents a prefix code. *)
+type t
+
+(** The type [word] is a list of booleans and represents a binary word. *)
+type word = bool list
+
+exception Coding_undef
+
+(** [empty ()] returns a new fresh and undefined prefix code. *)
+val empty : unit -> t
+
+(** [set code sym word] defines the encoding of [sym]. *)
+val set : t -> Source.byte -> word -> unit
+
+(** [unset code sym] removes the definition of the encoding of [sym]. *)
+val unset : t -> Source.byte -> unit
+
+(** [encode_sym code sym] returns the word which encodes [sym]. *)
+val encode_sym : t -> Source.byte -> word
+
+(** [encode_source code src] returns a long word which is the concatenation
+    of the encoding of all symbols contained in [src]. *)
+val encode_source : t -> Source.t -> word
