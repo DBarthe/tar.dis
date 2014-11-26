3d962ad94d51315b708df21eda2290e4710e5885
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2014-11-26 11:43:33 +0100

    Module PrefixCode decoding, change exception types

--- src/prefixCode.ml
+++ src/prefixCode.ml
@@ -8,7 +8,9 @@ type word = bool list
 
 type t = word array
 
-exception Coding_undef
+exception Coding_undef of int
+exception Undecodable of word
+exception Decoding_failure of (Source.t * word)
 
 let empty () = Array.make 256 []
 
@@ -27,7 +29,7 @@ let unset code sym =
 let encode_sym code sym =
   check_sym sym ;
   match code.(sym) with
-  | [] -> raise Coding_undef
+  | [] -> raise (Coding_undef sym)
   | c -> c
 
 (* tail-recursive version of flatten *)
@@ -40,6 +42,36 @@ let flatten_opti l =
 let encode_source code src =
   let words = List.map (encode_sym code) src in
   flatten_opti words
+
+let match_prefix word prefix =
+  ListExt.take (List.length prefix) word = prefix
+
+(* returns the first index of an array whom the value satisfys the predicat *)
+let index_for p ar =
+  let len = Array.length ar in
+  let rec aux i =
+    if i = len then None else
+    if p ar.(i) then Some i else
+    aux (i+1) in
+  aux 0
+
+let decode_next_sym code input =
+  match index_for (fun x -> x != [] && match_prefix input x) code with
+  | None -> raise (Undecodable input)
+  | Some sym ->
+      let input' = ListExt.drop (List.length code.(sym)) input in
+      (sym, input')
+
+let decode_input code input =
+  let rec aux accu = function
+  | [] -> List.rev accu
+  | inp ->
+    let (sym, inp') =
+      try decode_next_sym code inp with
+      | Undecodable _ ->
+        raise (Decoding_failure (List.rev accu, inp))
+    in aux (sym::accu) inp'
+  in aux [] input
 
 let show code =
   ignore begin
--- src/prefixCode.mli
+++ src/prefixCode.mli
@@ -10,7 +10,8 @@ type t
 (** The type [word] is a list of booleans and represents a binary word. *)
 type word = bool list
 
-exception Coding_undef
+exception Coding_undef of int
+exception Decoding_failure of (Source.t * word)
 
 (** [empty ()] returns a new fresh and undefined prefix code. *)
 val empty : unit -> t
@@ -21,12 +22,24 @@ val set : t -> Source.byte -> word -> unit
 (** [unset code sym] removes the definition of the encoding of [sym]. *)
 val unset : t -> Source.byte -> unit
 
-(** [encode_sym code sym] returns the word which encodes [sym]. *)
+(** [encode_sym code sym] returns the word which encodes [sym].
+  
+    @raise Coding_undef ([sym]) if the coding is not defined for [sym]. *)
 val encode_sym : t -> Source.byte -> word
 
 (** [encode_source code src] returns a long word which is the concatenation
-    of the encoding of all symbols contained in [src]. *)
+    of the encoding of all symbols contained in [src].
+
+    @raise Coding_undef (sym) if [sym] is a symbol if [src] that has no
+    defined coding.*)
 val encode_source : t -> Source.t -> word
+
+(** [decode_input code input] decodes [input] with [code] and returns the
+    source decoded.
+
+    @raise Decoding_failure (decoded, remaining) if the [input] can't be
+    decoded. *)
+val decode_input : t -> word -> Source.t
 
 (** [show code] print the code in the standard output *)
 val show : t -> unit
